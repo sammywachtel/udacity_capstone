6526a24208670f205f150aab7f0d4fda9cc3cc07
Sam Wachtel <subp@wachtel.us>
2019-09-27 19:31:01 +0000

    type: feature body: updated spectrogram generation code to be more flexible

--- utils/audio_utils.py
+++ utils/audio_utils.py
@@ -95,51 +95,37 @@ def create_mel_to_hz_plot(sample_rate):
 
 
 def create_spectrogram_parallelized(audio, sample_rate, audioname, directory,
-                                    batch_num, batch_i_num, batch_size, num_samples, mel):
+                                    batch_num, batch_i_num, batch_size, num_samples, mel, overwrite=False):
+    # Since we are potentially returning in more than one place
+    ret_val = batch_num * batch_size + batch_i_num+1, batch_num, num_samples
+    
+    # put together file name (will need the subdirectory below, so break it out)
+    subdirectory = directory + '/' + audioname.split('_')[0] + '/'
+    filename = subdirectory + audioname + '.jpg'
+    
+    # Skip the file if overwrite is False
+    if not overwrite:
+        if os.path.exists(filename):
+            return ret_val
     
     # Create the subsirectory for the instrument family if it doesn't exist
-    subdirectory = directory + '/' + audioname.split('_')[0] + '/'
     if not os.path.exists(subdirectory):
         os.makedirs(subdirectory)
         
     plt = create_spectrogram(audio, audioname, sample_rate, mel=mel, spect_only=True, figsize=[0.72,0.72])
-    filename = subdirectory + audioname + '.jpg'
     plt.savefig(filename, dpi=400, bbox_inches='tight',pad_inches=0)
     plt.close()    
     #fig.clf()
     #plt.close(fig)
     #plt.close('all')
-    #del plt
-    return batch_num * batch_size + batch_i_num+1, batch_num, num_samples
-
-def create_spectrogram_parallelized_bak(audio, sample_rate, audioname, directory,
-                                    batch_num, batch_i_num, batch_size, num_samples, mel):
-    
-    # Create the subsirectory for the instrument family if it doesn't exist
-    subdirectory = directory + '/' + audioname.split('_')[0] + '/'
-    if not os.path.exists(subdirectory):
-        os.makedirs(subdirectory)
-        
-    fig, axs = plt.subplots(1, 1, figsize=[0.72,0.72])
-    plt.axis('off')
-    axs.set_frame_on(False)
-    spect = librosa.feature.melspectrogram(y=audio, sr=sample_rate)
-    db = librosa.power_to_db(spect, ref=np.max)
-    librosa.display.specshow(db)
-    filename = subdirectory + audioname + '.jpg'
-    plt.savefig(filename, dpi=400, bbox_inches='tight',pad_inches=0)
-    plt.close()    
-    fig.clf()
-    plt.close(fig)
-    plt.close('all')
-    del filename,fig,ax,spect
-    return batch_num * batch_size + batch_i_num+1, batch_num, num_samples
+    del plt
+    return ret_val
 
 def create_spectrogram_parallelized_callback(ret):
-    if ret[0]%50 == 0:
+    if ret[0]%50 == 0 or ret[0] == ret[2]:
         print('\rprocessed {} files out of {} in {} batches'.format(ret[0], ret[2], ret[1]), end="")
         
-def write_spectograms_parallelized(tfrecord_file_name, directory, batch_size = 50, mel=False):
+def write_spectograms_parallelized(tfrecord_file_name, directory, batch_size = 50, mel=False, overwrite=False):
     num_tfrecords = general_utils.get_tfrecord_count(tfrecord_file_name)
 
     with tf.Graph().as_default():
@@ -152,7 +138,7 @@ def write_spectograms_parallelized(tfrecord_file_name, directory, batch_size = 5
             dataset = dataset.map(parse_func)
 
             #set to different number to take fewer samples for testing
-            num_samples = num_tfrecords #50 
+            num_samples = num_tfrecords #50
             
             # no need to shuffle as we are only generating images
             #dataset = dataset.shuffle(buffer_size=num_tfrecords).take(num_samples)
@@ -173,7 +159,7 @@ def write_spectograms_parallelized(tfrecord_file_name, directory, batch_size = 5
                 pool = mp.Pool(mp.cpu_count())
                 [pool.apply_async(create_spectrogram_parallelized, args=(batch['audio'][i], batch['sample_rate'][i],
                                                                    batch['note_str'][i].decode('utf-8'), directory,
-                                                                   batch_num, i, batch_size, num_samples, mel),
+                                                                   batch_num, i, batch_size, num_samples, mel, overwrite),
                                   callback=create_spectrogram_parallelized_callback
                                   ) for i in range(item_cnt_in_batch)]
                 pool.close()
